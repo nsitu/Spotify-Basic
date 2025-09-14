be9836dea0406c9d13eb1c8ea8ac18fd290ea58a
Harold Sikkema <harold@nsitu.ca>
2025-09-14 13:22:55 -0400

    change from recommendations endpoint to latest releases endpoint

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
- const main = document.querySelector('main')
+const main = document.querySelector('main')
 
 fetch('/token')
   .then(response => response.json())
@@ -9,32 +9,40 @@ fetch('/token')
       headers: {
         Authorization: 'Bearer ' + token.access_token
       }
-    }   
-      // see also: https://developer.spotify.com/documentation/web-api/reference/get-recommendations
-      const url = new URL('https://api.spotify.com/v1/recommendations')
-      url.searchParams.set('seed_artists', '4NHQUGzhtTLFvgF5SZesLK')
-      url.searchParams.set('seed_genres','classical,country')
-      url.searchParams.set('seed_tracks','0c6xIDDpzE81m2q797ordA')
-    
+    }
+    // see also: https://developer.spotify.com/documentation/web-api/reference/get-recommendations
+    const url = new URL('https://api.spotify.com/v1/browse/new-releases')
+
     // Fetch 
     fetch(url, options)
-      .then(response => response.json()) 
-      .then(response => { 
+      .then(response => response.json())
+      .then(response => {
         console.log(response)
-        display(response.tracks) 
+        display(response.albums?.items)
       })
       .catch(err => console.error(err))
-     
-      
+
+
   })
 
 
- const display = (tracks) => { 
-    tracks.forEach(track =>{
-      const div = document.createElement('div')
-      div.innerHTML = 
-        `<h3>${track.name}</h3>
-        <p>Popularity: ${track.popularity}</p>`
-      main.appendChild(div)
-    })
- }
+const display = (items) => {
+  items.forEach(item => {
+    const div = document.createElement('div')
+    // Find the largest image (highest width)
+    let largestImg = null;
+    if (item.images && item.images.length > 0) {
+      largestImg = item.images.reduce((max, img) => img.width > max.width ? img : max, item.images[0]);
+    }
+    // List contributing artists
+    let artists = '';
+    if (item.artists && item.artists.length > 0) {
+      artists = '<p>Artists: ' + item.artists.map(a => `<a href="${a.external_urls.spotify}" target="_blank">${a.name}</a>`).join(', ') + '</p>';
+    }
+    div.innerHTML =
+      `${largestImg ? `<img src="${largestImg.url}" alt="${item.name}" style="max-width:100%;height:auto;" />` : ''}
+      <h3>${item.name}</h3> 
+      ${artists}`
+    main.appendChild(div)
+  })
+}
